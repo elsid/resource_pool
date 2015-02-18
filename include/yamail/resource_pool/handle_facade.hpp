00923d48ddaa09b33e6f376e8698d642b0da33b7
Roman Siromakha <elsid@yandex-team.ru>
2015-02-18 20:48:06 +0400

    Change handle_facade accordingly handle interface

--- include/yamail/resource_pool/handle_facade.hpp
+++ include/yamail/resource_pool/handle_facade.hpp
@@ -9,15 +9,16 @@ namespace resource_pool {
 template <class ResourcePool>
 class handle_facade {
 public:
-    typedef ResourcePool resource_pool;
-    typedef typename resource_pool::value_type value_type;
-    typedef boost::shared_ptr<typename resource_pool::handle> handle_ptr;
+    typedef ResourcePool pool;
+    typedef typename pool::handle::value_type value_type;
+    typedef typename pool::handle::pointer pointer;
+    typedef boost::shared_ptr<typename pool::handle> handle_ptr;
 
     handle_facade(const handle_ptr& handle) : _handle(handle) {}
     virtual ~handle_facade() {}
 
     handle_ptr handle() const { return _handle; }
-    boost::system::error_code error() const { return _handle->error(); }
+    bool unusable() const { return _handle->unusable(); }
     bool empty() const { return _handle->empty(); }
     value_type& get() { return _handle->get(); }
     const value_type& get() const { return _handle->get(); }
@@ -28,6 +29,7 @@ public:
 
     void recycle();
     void waste();
+    void reset(pointer res) { _handle->reset(res); }
 
 protected:
     virtual void before_recycle() {}
