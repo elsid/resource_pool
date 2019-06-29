7302cc22687d760f5e46204d1f78d674f62ffcb1
JonasS <jonass@dev.jsje.de>
2019-06-29 17:29:49 +0200

    Modern CMake - switched to target based CMake - use target-based cmake for exmaples - tests & benchmarks still use legacy CMake, but import the targets - added proper CPack configuration files - added Conan recipe

---
+++ scripts/travis/conan.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+pip install --user virtualenv
+virtualenv conan-temp
+. conan-temp/bin/activate
+pip install conan
+conan profile new --detect default
+
+cd "${TRAVIS_BUILD_DIR}"
+
+PKG_REPO="yandex/testing"
+
+
+conan export . "${PKG_REPO}"
+
+PKG_NAME="$(conan inspect --raw name .)/$(conan inspect --raw version .)"
+PKG_ID="${PKG_NAME}@${PKG_REPO}"
+echo "package-id: ${PKG_ID}"
+
+conan install "${PKG_ID}" --build=missing
+conan test test_package "${PKG_ID}"
